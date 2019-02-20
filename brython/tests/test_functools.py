f14a7644a0ece13398281a71ae15ad22b3e18839
rdecelis <rdecelis@stt-systems.com>
2019-02-20 10:43:25 +0100

    ♻️⚠️ Clean unused code

--- brython/tests/test_functools.py
+++
@@ -1,9 +0,0 @@
-# issue 789
-
-from functools import *
-@total_ordering
-class Test:
-    def __eq__(self, other):
-        pass
-    def __lt__(self, other):
-        pass
--- brython/tests/test_urllib.py
+++
@@ -1,5 +0,0 @@
-import urllib.parse
-
-assert urllib.parse.unquote("foo%20bar") == "foo bar"
-
-print('passed all tests')
